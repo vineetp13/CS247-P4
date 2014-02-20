b7dee3e5098373c3d93bda56e8488a40b68717ee
William Chidyausiku <wchid@stanford.edu>
2014-02-20 07:02:40 -0800

    further mods

--- Websites/team_dewey_website/config/routes.rb
+++ Websites/team_dewey_website/config/routes.rb
@@ -1,4 +1,5 @@
 TeamDeweyWebsite::Application.routes.draw do
+  get "users/index"
   resources :graphs
 
   resources :topics
@@ -11,7 +12,7 @@ TeamDeweyWebsite::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
