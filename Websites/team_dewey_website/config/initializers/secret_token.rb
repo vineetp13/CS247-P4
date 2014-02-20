62d1faa201ab0e160dee7679a231d53f7fba4119
William Chidyausiku <wchid@stanford.edu>
2014-02-20 05:38:27 -0800

    First commit. Set up structure.

---
+++ Websites/team_dewey_website/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeamDeweyWebsite::Application.config.secret_key_base = '97da4e295c3fb2b0c94be65d1884069638fffaadd7fe36fe35ec2fc46d0c8923b84ee4bd642a884a326b1418c039856fcedf8e5e771f1826614d7dfa93f47cd8'
---
+++ Websites/team_dewey_website/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TeamDeweyWebsite::Application.config.session_store :cookie_store, key: '_team_dewey_website_session'
