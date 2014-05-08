9928cbdd45389e5b0b985364dd59eb157dfbdaf1
Julia Cambre <jcambre@stanford.edu>
2014-05-08 15:27:51 -0700

    Fixed broken messaging

--- turn-taking.js
+++ turn-taking.js
@@ -810,7 +810,7 @@ function setNumParticipantsNeeded() {
   var num_participants = gapi.hangout.getParticipants().length;
   var num_needed_participants = 3 - (num_participants);
   if (num_needed_participants <= 0) {
-    document.getElementById("num_participants_needed").innerHTML = "You're all set for your discussion! Whenever everyone is ready, have someone click the \"Start Discussion\" button to initiate the discussion.";
+    document.getElementById("pending_participants").innerHTML = "You're all set for your discussion! Whenever everyone is ready, have someone click the \"Start Discussion\" button to initiate the discussion.";
   } else {
     document.getElementById("num_participants_needed").innerHTML = num_needed_participants;
   }
