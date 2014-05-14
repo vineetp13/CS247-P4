6ede963c2db8257ccdf19869440dd96f8ca1aeb3
Julia Cambre <jcambre@stanford.edu>
2014-05-14 00:20:14 -0700

    Adding new configuration for instructor/administrator priviledges

--- turn-taking.js
+++ turn-taking.js
@@ -431,6 +431,13 @@ function init() {
           recenterCanvas();
       });
 
+      // If the current participants are "administrators" (acting as instructors for the purposes of the demo), give them access to the Start/Stop discussion buttons
+      var localParticipantEmail = gapi.hangout.getLocalParticipantID();
+      console.log("My local participant ID is: " + gapi.hangout.getLocalParticipantID());
+      if (localParticipantEmail == "jscambre@gmail.com" || localParticipantEmail == "wjkchid@gmail.com" || localParticipantEmail == "kdumovic@gmail.com") {
+        document.getElementById("buttons").innerHTML = '<center><button type="button" class="btn btn-success disabled" id="start_discussion_btn">Start Think-Pair-Share</button><button type="button" class="btn btn-primary" id="restart_discussion_btn" style="display:none;">Restart Think-Pair-Share</button></center>';
+      }
+
       //Determine whether the just-added local participant is a turn reporter (there are two per session)
       listenForTurnReporting();
 
